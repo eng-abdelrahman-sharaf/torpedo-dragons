e26784cd8c437f864b05b88178dda2f5916c4c05
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-08-18 19:23:52 +0300

    implementing main

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "system.h"
+
+int main() {
+	System sys;
+	sys.mainLoop();
+    // Logger *logger = Logger::init("logging.txt");
+    // Customer customer("1", "ali");
+    // logger->logPurchase(&customer , 120);
+}
